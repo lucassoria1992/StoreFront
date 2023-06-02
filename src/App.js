4fa0975d804afd4edfc5d4f58ad9731de8d365aa
Lucas Soria <lucassoria1992@gmail.com>
2023-06-02 06:39:13 -0300

    router

--- src/App.js
+++ src/App.js
@@ -2,18 +2,14 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Layout from './Components/Header';
-import FormularioCarga from './Components/Formulario';
-import ProductItemsList from '../src/Conteiners/Productos.jsx';
-import FormularioEdicion from './Components/FormularioEdicion';
+import Home from './Pages/Home'
 
 function App() {
   return (
     <BrowserRouter>
       <Layout/>
         <Routes>
-          <Route path='/list' element={<ProductItemsList />} />
-          <Route path='/formCarga' element={<FormularioCarga/>} />
-          <Route path='/formEdit' element={<FormularioEdicion/>} />
+          <Route path='/' element={<Home/>} />
         </Routes>
     </BrowserRouter>
     
--- src/Conteiners/Productos.jsx
+++ src/Conteiners/Productos.jsx
@@ -2,6 +2,7 @@
 import UseGetItems from '../hooks/UseGetItems';
 import ProductItem from '../Components/Producto';
 import { Container, Row } from 'react-bootstrap';
+import FormularioCarga from '../Components/Formulario';
 
 
 const ProductItemsList = () => {
@@ -10,9 +11,11 @@ const ProductItemsList = () => {
     return (
         <Container>
             <div>
-                <Row md={4} className=" container d-flex">
+            
+                <Row md={3} className=" container d-flex align-items-center " >
+                <FormularioCarga  style={{ Width: 'max-content' } }  className="m-5"/>
                     {items.map(item => (
-                        <ProductItem key={item.id} item={item} />
+                        <ProductItem key={item.id} item={item} style={{ Width: 'max-content' } }/>
                         
                     )
                     )
